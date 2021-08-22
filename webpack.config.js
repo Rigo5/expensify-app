d285e3d21056aa4d58ad9b7876a3d291aad3e974
Michele <micheleprisco1@gmail.com>
2021-08-22 11:37:20 +0200

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,54 @@
 //dove mettiamo il bundle file ? 
 
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-            { test: /\.s?css$/, use: ['style-loader', 'css-loader', 'sass-loader'] }
-        ]
-    },
-    //ci mostra esattamente deov gli errori sono (ovvimente dalla console)
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        //questo gli dice che stiamo gestendo le routes attraverso il client e ricadano sul file index
-        historyApiFallback : true
-    }
-};
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('style.css');
+
+    console.log('env ', env);
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+                { test: /\.s?css$/, use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader : 'css-loader',
+                            options:{
+                                sourceMap : true
+                            }
+                        },
+                        {
+                            loader : 'sass-loader',
+                            options : {
+                                sourceMap : true
+                            }
+                        }
+                    ]
+                }) }
+            ]
+        },
+        plugins : [
+            CSSExtract
+        ],
+        //ci mostra esattamente deov gli errori sono (ovvimente dalla console)
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            //questo gli dice che stiamo gestendo le routes attraverso il client e ricadano sul file index
+            historyApiFallback : true
+        }
+    };
+}
